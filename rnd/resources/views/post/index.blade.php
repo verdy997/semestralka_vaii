4e1edafdbd30bbc3364d6eb389a6a1735faa79d1
Matúš Veróny <matverony@gmail.com>
2021-01-20 16:28:44 +0100

    oprava strankovania

--- rnd/resources/views/post/index.blade.php
+++ rnd/resources/views/post/index.blade.php
@@ -2,26 +2,6 @@
 
 @section('content')
     @include('post.post_data')
-    <script>
-        $(document).ready(function (){
-            $(document).on('click', '.pagination a', function (event) {
-                event.preventDefault();
-                var page = $(this).attr('href').split('page=')[1];
-                getMorePosts(page);
-            });
 
-            function getMorePosts(page)
-            {
-                $.ajax({
-                    type:"GET",
-                    url:"/post/getMorePosts"+page,
-                    success:function(data)
-                    {
-                        $('#post_data').html(data);
-                    }
-                });
-            }
-        });
-    </script>
 @endsection
 
